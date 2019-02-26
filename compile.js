44cfaeff4f0c4a4bde689295cfc1caac28853996
youseffyp <polk@usc.edu>
2019-02-26 09:46:34 -0800

    beginning testing

--- compile.js
+++ compile.js
@@ -6,4 +6,8 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+var y = solc.compile(source, 1).contracts[':Inbox'];
+
+module.exports = y;
+
--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
 contract Inbox {
 
@@ -13,5 +13,8 @@ contract Inbox {
 
         message = newMessage;
     }
-
+    
+    function getMessage () public view returns (string) {
+        return message;
+    }
 }
